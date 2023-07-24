4a237ff4f3d13bf84ccf3f4c8692b3be74bdb6e9
leebeanbin <67886181+leebeanbin@users.noreply.github.com>
2023-07-24 22:21:58 +0900

    Update data_labeled.m
    오늘은 배경을 제거하고 나머지를 구하는 프로그램을 구성하는 것을 시도

--- mat/data_labeled.m
+++ mat/data_labeled.m
@@ -1,44 +1,60 @@
 % 내가 보려고 하는 사진의 경로
-folder_path = '/Users/leejungbin/Desktop/train_set/postProcessing/';
-% compare_path = '/Users/leejungbin/Desktop/train_set/test/';
+folder_path = '/Users/leejungbin/Documents/MATLAB/results/';
 
-% 결과를 추가할 txt 파일 경로
-file_list = dir(fullfile(folder_path, '*.tif'));
+% 비교 폴더
+compare_path = '/Users/leejungbin/Documents/MATLAB/postProcessing/';
 
-% 객체가 없는 사진의 결과 이미지 미리 이진화
-% comp_path1 = fullfile(compare_path, '0001.tif');
-% img2_data = imread(comp_path1);
-% 
-% threshold = 0.5;
-% binary2_image = imbinarize(img2_data,threshold);
-% labeled_image2 = bwlabel(binary2_image, 8); % 객체를 라벨링
-% num_bubbles2 = max(labeled_image2(:));
+output_path = '/Users/leejungbin/Documents/MATLAB/renew/';
 
-% 결과를 추가할 txt 파일 경로
-output_file_path = fullfile(folder_path, 'existing_bubbles_count.txt');
-fid = fopen(output_file_path, 'a');  % 파일 열기 (추가 모드)
+% 디렉토리에서 '*.tif' 확장자를 가진 파일들의 목록을 가져옴
+file_list = dir(fullfile(folder_path, 'postProcessing1000.tif'));
+compare_file_list = dir(fullfile(compare_path, 'pP1000.tif'));
 
-
-for i = 1: length(file_list) % 파일 개수 확인
+for i = 1:length(file_list) % 파일 개수 확인
     file_name = file_list(i).name;
+    compare_name = compare_file_list(i).name;
+
     file_path1 = fullfile(folder_path, file_name);
+    compare_file_path1 = fullfile(compare_path, compare_name);
 
     img_data = imread(file_path1);
-    
-%     binary_image = imbinarize(img_data);
-    
-    % 객체 탐지 (버블을 식별)
-    labeled_image = bwlabel(img_data,8); % 객체를 라벨링
-    num_bubbles = max(labeled_image(:));
-    fprintf('%s: 버블의 개수 = %d\n', file_name, num_bubbles);
+    compare_img_data = imread(compare_file_path1);
 
-    % 라벨링된 객체를 다른 색상으로 표시하여 이미지 출력
-    colored_labels = label2rgb(labeled_image, 'hsv', 'k', 'shuffle');
+    % 이미지 white & black 반전
+    inverted_image_data = ~img_data;
+    inverted_compare_image_data = ~compare_img_data;
+
+    % 객체 탐지 (버블을 식별)
+    labeled_image = bwlabel(inverted_image_data, 8); % 객체를 라벨링
+    relabeled_image = bwlabel(inverted_compare_image_data, 8); % 객체를 라벨링
+
+    % 두 이미지의 라벨링이 동일한지 확인
+    common_labels = labeled_image & relabeled_image;
+
+
+    common_image = bwlabel(common_labels, 8);
+
+    % 라벨링된 객체 개수 세기
+    num_common_objects = max(common_image(:));
+
+    % 결과 출력
+    fprintf('공통으로 라벨링된 객체 개수(%d) : %d\n', i , num_common_objects);
+
+    % 라벨링된 객체만 표시하는 마스크 생성 (흰색 배경)
+    labeled_mask = ~labeled_image;
+    relabeled_mask = ~relabeled_image;
+
+    result = relabeled_image - relabeled_mask; 
+
+    % 라벨링된 객체만 추출하여 컬러 이미지 출력
+    colored_labels = label2rgb(labeled_mask, 'jet', 'w', 'shuffle'); % 'jet' colormap 사용, 배경은 흰색('w')
     figure;
     imshow(colored_labels);
+
+    colored_labelsi = label2rgb(result, 'jet', 'w', 'shuffle'); % 'jet' colormap 사용, 배경은 흰색('w')
+    figure;
+    imshow(colored_labelsi);
+
 end
 
-fclose(fid);
 
-% I tried to make code for circles detection but, this code just detected other areas except points
-% where I would like to see. So, I'm just going to keep changing this one to do more clear.
